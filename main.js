40f17eeec841c3138968a9257386c6d4e5894345
Ripal Patel <ripal212007@gmail.com>
2018-10-27 12:38:48 -0500

    Working

---
+++ main.js
@@ -0,0 +1,36 @@
+
+
+const requestData = ()=>{
+    return new Promise ((resolve, reject)=>{
+   
+    $.ajax('https://data.nashville.gov/resource/8zc7-2afq.json')
+        .done((data)=>{
+            resolve(data);
+            console.log(data)
+        }).fail((error)=> {
+            reject(error);
+        })
+    
+    })
+}
+const writeToDom = (resources) => {    
+    let domString = '';
+   resources.forEach((resource)=>{         
+       domString += `<div>
+                       <h3>${resource.contact}</h3>
+                       <p>${resource.contact_type}</p>
+                       <p>${resource.location_1_address}</p>
+                   </div>
+                   `
+})
+$('#resources').html(domString);
+}
+
+const displayData = () => {
+    requestData().then((data)=>{
+        writeToDom(data);
+    })
+}
+
+displayData();
+
